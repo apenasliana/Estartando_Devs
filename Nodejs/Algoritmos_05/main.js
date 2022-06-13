9580eed7415f7f924ae857dfbe79eeb83cd9e139
apenasliana <lianarghini@gmail.com>
2022-06-12 21:29:22 -0300

    algoritmos 05

---
+++ Nodejs/Algoritmos_05/main.js
@@ -0,0 +1,14 @@
+const getDirectoryDataPath = () => {
+
+}
+
+const createFile = async (data, fileName) => {
+
+}
+const renameFile = async (fileName, newName) => {
+
+}
+
+const deleteFile = async (fileName) => {
+    
+}
